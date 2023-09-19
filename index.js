7df00b87311f6bc8a9880d7e4acc6143ee3b7d6d
Enjamuri Sagar <enjamurisagar@gmail.com>
2023-09-19 23:59:36 +0530

    fifth1

--- index.js
+++ index.js
@@ -42,7 +42,7 @@ const storage = multer.diskStorage({
   },
   filename: function (req, file, cb) {
     // const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
-    cb(null, file.originalname);
+    cb(null, file.name);
   },
 });
 
